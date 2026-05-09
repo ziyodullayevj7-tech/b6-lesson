b337360041f72b5e7caf3bf2ca8f508e039c6afb
ziyodullayevj7 <ziyodullayevj7@gmail.com>
2026-05-09 20:41:39 +0500

    hello madefa

--- src/Main.java
+++ src/Main.java
@@ -10,4 +10,5 @@ void main() {
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         IO.println("i = " + i);
     }
+    //moalkdfhsdf
 }
